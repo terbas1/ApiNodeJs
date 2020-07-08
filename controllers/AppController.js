f8446e598f9bf2ec7b3e89ef1e3e25cbb70b975f
Edwin Daniel Baltazar Terbullino <edwin.baltazar@tecsup.edu.pe>
2020-07-08 01:38:29 -0500

    first commit

---
+++ controllers/AppController.js
@@ -0,0 +1,68 @@
+let db = require('../models/dbconexion');
+
+let productos = {
+  listar( req, res ){
+    let sql = "SELECT * FROM productos";
+    db.query(sql,function(err, result){
+      if( err ){
+        console.log(err);
+        res.sendStatus(500);
+      }else{
+        res.json(result);
+      }
+    });
+  },
+  store( req, res ){
+    val_nombre = req.body.Name;
+    val_desck   = req.body.Notes;
+    val_precio = req.body.Done;
+    let sql = "INSERT INTO productos(Name,Notes,Done) VALUES(?,?,?)";
+    db.query(sql,[val_nombre,val_desck,val_precio],function(err, newData){
+      if(err){
+        console.log(err);
+        res.sendStatus(500);
+      }else{
+        res.json(newData);
+      }
+    });
+  },
+  show( req, res ){
+    val_id = req.params.id;
+    let sql = "SELECT * FROM productos WHERE id=?";
+    db.query(sql,[val_id],function(err, rowData){
+      if(err){
+        console.log(err);
+        res.sendStatus(500);
+      }else{
+        res.json(rowData);
+      }
+    });
+  },
+  edit( req, res ){
+    val_id = req.body.id;
+    val_nombre = req.body.Name;
+    val_desck   = req.body.notes;
+    val_precio = req.body.done;
+    let sql = "UPDATE productos SET Name=?, Notes=?, Done=? WHERE id=?";
+    db.query(sql,[val_nombre,val_desck,val_precio,val_id],function(err, newData){
+      if(err){
+        res.sendStatus(500);
+      }else{
+        res.json(newData);
+      }
+    });
+  },
+  delete( req, res ){
+    val_id = req.params.id;
+    let sql = "DELETE FROM productos WHERE id=?";
+    db.query(sql,[val_id],function(err, newData){
+      if(err){
+        res.sendStatus(500);
+      }else{
+        res.sendStatus(200);
+      }
+    });
+  }
+}
+
+module.exports = productos;
